a3d3bf5e3a68c7ee023285885631f158fbc6c1ed
Alexander Mendivil <alexanderima@outlook.es>
2022-05-08 15:38:14 -0700

    validar si es estudiante o empleado

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const io = require("socket.io")(process.env.PORT || 3000,{
+const { infoStudent } = require("./Excel_Handle/ExcelHandling")
+const { infoEmployee } = require("./Excel_Handle/ExcelHandling")
+
+const PORT = process.env.PORT || 3000;
+
+const io = require("socket.io")(PORT,{
     cors:{
         origin: false
     }
@@ -6,7 +11,16 @@ const io = require("socket.io")(process.env.PORT || 3000,{
 
 io.on("connection", socket =>{
 
-    socket.on("send-num_control", (numero_control) =>{
+    socket.on("send-num_control", (badgeNumber) =>{
         
+        if(badgeNumber.toString().length > 4){
+            const resultInfoStudent = infoStudent(badgeNumber);
+            socket.broadcast.emit("info-alumno", resultInfoStudent);
+        }else{
+            const resultInfoEmployee = infoEmployee(badgeNumber);
+            socket.broadcast.emit("info-empleado", resultInfoEmployee);
+
+        }
+
     })
 })
