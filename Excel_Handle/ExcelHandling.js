d26bb90d015b668c5c111b243cc77ab1acd5f0a6
Alexander Mendivil <alexanderima@outlook.es>
2022-05-08 15:38:43 -0700

    Retorna la información del alumno o empleado especifico

--- Excel_Handle/ExcelHandling.js
+++ Excel_Handle/ExcelHandling.js
@@ -1,13 +1,46 @@
 const XLSX = require("xlsx");
 
-function leerExcel( ruta ){
+const readExcelStudent = ( ruta ) => {
     const workbook = XLSX.readFile(ruta);
     const workbookSheet = workbook.SheetNames;
     
     const sheet = workbookSheet[0];
-    const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-    console.log(dataExcel);
+    const dataExcelStudent = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+    return dataExcelStudent;
 }
-leerExcel("0_inscritos_detalle_ESCOLARES20221.xls");
 
+export const infoStudent = (badgeNumber) => {
+    const infoStudents = readExcelStudent("0_inscritos_detalle_ESCOLARES20221.xls");
 
+    const info = search(infoStudents, infoStudents.length, badgeNumber)
+    return info;
+}
+
+export const infoEmployee = (badgeNumber) => {
+    const infoEmployees = readExcelStudent("Personal-tarjeta20221.xls");
+
+    const info = search(infoEmployees, infoEmployees.length, badgeNumber)
+    return info;
+}
+
+const search = (arr, len, value) => {
+
+    if (arr[len - 1].no_de_control == value)
+            return arr[i];
+    
+        let backup = arr[len - 1];
+        arr[len - 1].no_de_control = value;
+        for (let i = 0;; i++) {
+                
+            if (arr[i] == value) {
+                    
+                arr[len - 1].no_de_control = backup;
+    
+                if (i < len - 1)
+                    return arr[i];
+    
+                return "Not Found";
+            }
+
+        }
+}
